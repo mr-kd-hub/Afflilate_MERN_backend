b28b94a1a4fb71421d5cdfb2d8866f24a1f65eb2
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2021-03-31 22:06:35 +0530

    removed unwanted env import

--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-dotenv.config({path:'../.env'});
 module.exports.tokenGenerator = (id) => {
   try {
     const token = jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '24h' });
